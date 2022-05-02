98ee75dffee64027d71b0fd0ecbe0b79ab3d3206
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-05-02 15:55:26 -0300

    changed the data, formatting it to dd/mm/yyyy

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -8,20 +8,16 @@ class NegociacaoController {
         this.#inputData = $("data");
         this.#inputQuantidade = $("quantidade");
         this.#inputValor = $("valor");
-
-    }
+    };
     adiciona(event) {
         event.preventDefault();
-
-        let data = new Date(
-            ...this.#inputData.value.split("-").map((item, index) => item - index % 2)
-        )
-
+        let helper = new FormataData();
+        const data = helper.textoParaData(this.#inputData.value);
+        console.log(helper.dataParaTexto(data));
         let negociacao = new Negociacao(
             data,
             this.#inputQuantidade.value,
             this.#inputValor.value,
         );
-        console.log(negociacao);
     }
 }
---
+++ client/js/app/helpers/FromataData.js
@@ -0,0 +1,10 @@
+class FormataData {
+    textoParaData(texto) {
+        return new Date(...texto.split("-").map((item, index) => item - index % 2));
+    }
+    dataParaTexto(data) {
+        return data.getDate() +
+            "/" + (data.getMonth() + 1) +
+            "/" + data.getFullYear();
+    }
+}
