01584c27f1639388353fc5724f45fd4379fdcc4e
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-05-03 22:17:20 -0300

    semantics adjustment

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -3,11 +3,13 @@ class NegociacaoController {
     #inputData;
     #inputQuantidade;
     #inputValor;
+    #listaNegociacoes;
     constructor() {
         const $ = document.getElementById.bind(document);
         this.#inputData = $("data");
         this.#inputQuantidade = $("quantidade");
         this.#inputValor = $("valor");
+        this.#listaNegociacoes = new ListaNegociacoes();
     };
     adiciona(event) {
         event.preventDefault();
@@ -18,6 +20,6 @@ class NegociacaoController {
             this.#inputQuantidade.value,
             this.#inputValor.value,
         );
-        console.log(negociacao);
+        this.#listaNegociacoes.adiciona(negociacao);
     }
 }
---
+++ client/js/app/models/ListaNegociacoes.js
@@ -0,0 +1,13 @@
+class ListaNegociacoes {
+
+    #negociacoes;
+    constructor() {
+        this.#negociacoes = []
+    }
+    adiciona(negociacao) {
+        this.#negociacoes.push(negociacao);
+    };
+    get negociacoes() {
+        return this.#negociacoes
+    }
+}
