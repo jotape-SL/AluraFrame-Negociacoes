d85732c5a0ce92d4803c8bd37696775dc50b52a1
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-05-08 22:55:52 -0300

    little touch on template instance for View

--- client/js/app/views/View.js
+++ client/js/app/views/View.js
@@ -4,7 +4,9 @@ class View {
     constructor(elemento) {
         this.#elemento = elemento;
     }
-
+    template() {
+        throw new Error("O metodo template precisa ser implementado!")
+    }
     update(model) {
         this.#elemento.innerHTML = this.template(model);
     }
